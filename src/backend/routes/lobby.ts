f60d9abec2c219df5eb0f66d2d014100efaf97ed
Salgod25 <Salgod25@outlook.com>
2025-12-04 02:40:28 -0800

    Lobby

--- src/backend/routes/lobby.ts
+++ src/backend/routes/lobby.ts
@@ -5,6 +5,15 @@ const router = express.Router();
 router.get("/", (request, response) => {
   const { user } = request.session;
 
+  /* Test User
+  const user = request.session.user || {
+    id: 1,
+    username: "TestUser",
+    email: "test@test.com",
+    created_at: new Date()
+  };
+  */
+
   response.render("lobby/lobby", { user });
 });
 
